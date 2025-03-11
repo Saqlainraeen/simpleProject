6c4f32b091cbfa0e013fe3ed512a771aa6738fb9
Saqlain <Saqlainraeen2000@gmail.com>
2025-03-11 11:33:47 +0530

    message

---
+++ src/com/Main.java
@@ -0,0 +1,16 @@
+package com;
+
+import com.model.Book;
+
+public class Main{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Book book =new Book();
+		book.setTitle("Java Book");
+		book.setAuthor("Saqlain");
+		System.out.println(book);
+
+	}
+
+}
